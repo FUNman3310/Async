b9b58313e7b118fb1fd775625569f128703c82d9
FUNman3310 <tu7ldh0l3@code.edu.az>
2022-12-09 05:17:44 +0400

    complited

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿
+
+namespace Async
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Thread thread1 = new Thread(Loop1);
+            //Thread thread2 = new Thread(Loop2);
+            //thread1.Start();
+            //thread1.Join();
+            //thread2.Start();
+
+
+            //static void Loop1()
+            //{
+            //    for (int i = 0; i < 100; i++)
+            //    {
+            //        Thread.Sleep(7000);
+            //        Console.Write("A");
+            //    }
+            //}
+
+            //static void Loop2()
+            //{
+            //    for (int i = 0; i < 100; i++)
+            //    {
+            //        Thread.Sleep(1000);
+            //        Console.Write("B");
+            //    }
+            //}
+
+
+            Program program = new Program();
+            //program.Incrementor();
+            //program.Decrementor();
+
+            Thread thread1 = new Thread(program.Incrementor);
+            Thread thread2 = new Thread(program.Decrementor);
+            Console.WriteLine(program.Counter);
+
+            thread1.Start();
+            thread1.Join();
+            thread2.Start();
+            thread2.Join();
+        }
+
+        public int Counter { get;  set; }
+        private object LockObj1 = new object();
+        private object LockObj2 = new object();
+
+        void Incrementor()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                lock (LockObj1)
+                {
+                    lock (LockObj2) 
+                    {
+                        Counter++;
+
+                    }
+
+                }
+            }
+        }
+        void Decrementor()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                lock (LockObj2)
+                {
+                    lock (LockObj1)
+                    {
+                        Counter--;
+
+                    }
+                }
+                
+            }
+        }
+
+
+
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,46 @@
+﻿namespace AsyncTask
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //var task = Task.Run(() =>
+            //{
+            //    Console.WriteLine("Salam, AB102");
+            //});
+            //task.Wait();
+            //Console.ReadLine();
+
+
+            var data =  GetStringDataAsync();
+            //Console.WriteLine(data);
+
+            while (data.IsCompleted)
+            {
+                Console.WriteLine("Conntinue");
+            }
+            Console.WriteLine("Finish");
+
+
+            Console.WriteLine("Hloooooow");
+            //var data = await GetStringData();
+            //Console.WriteLine(data);
+
+            
+        }
+
+        static async Task<string> GetStringDataAsync()
+        {
+            HttpClient client = new HttpClient();
+            return await client.GetStringAsync("https://github.com/FUNman3310/Async");
+        }
+
+        static async Task<string> GetStringData()
+        {
+            HttpClient client = new HttpClient();
+            var data = client.GetStringAsync("https://github.com/FUNman3310/Async").Result;
+            return data;
+        }
+
+    }
+}
